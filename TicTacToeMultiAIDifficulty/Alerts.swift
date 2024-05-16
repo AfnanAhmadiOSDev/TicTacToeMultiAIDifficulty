e206a69d332ff4e0496487c357e1fab9b5720531
AfnanAhmadiOSDev <afnan.ahmad.iosdev@gmail.com>
2024-05-16 15:41:36 +0500

    before making the AI smarter (?)

---
+++ TicTacToeMultiAIDifficulty/Alerts.swift
@@ -0,0 +1,18 @@
+//
+//  Alerts.swift
+//  TicTacToeMultiAIDifficulty
+//
+//  Created by Afnan Ahmad on 16/05/2024.
+//
+
+import SwiftUI
+
+struct Alerts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Alerts()
+}
